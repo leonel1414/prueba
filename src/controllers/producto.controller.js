87304a5b9dfce16b838dbf49e9d08d51bea85640
leonel1414 <leoo9211@hotmail.com>
2025-08-21 11:34:40 -0300

    store products

--- src/controllers/producto.controller.js
+++ src/controllers/producto.controller.js
@@ -1,14 +1,29 @@
 const querystring = require('querystring');
-
+const model = require('../models/product');
 
 const create = (req,res) =>{
     res.render('productos/create');
 }
 
-const store = (req,res) => {
+const store = async (req,res) => {
     const {name} = req.body;
 
-}
+    try {
+
+        const result = await model.store(name);
+
+        console.log(result);
+
+        res.redirect('/productos');
+
+    } catch (error) {
+
+        console.error(error);
+
+        res.status(500).send('Error al crear el producto');
+    }
+
+};
 
 const getProductos =  (req, res) => {
     const query = querystring.stringify(req.query);
--- src/models/product.js
+++ src/models/product.js
@@ -1,12 +1,19 @@
 const pool = require('./mysql');
 
 const store = async (name) =>{
-    const sql = 'INSERT INTO productos (name) VALUES (?)'
+    const sql = 'INSERT INTO products (name) VALUES (?)'
 
     try{
         const [result] = await pool.query(sql, [name]);
-        return result
+        
+        return result;
+
     } catch (error) {
+
         throw error;
     }
 }
+
+module.exports = {
+    store
+};
